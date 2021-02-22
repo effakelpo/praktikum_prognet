ae33b41120c025c207002c13c8205a4f22aecfdc
effakelpo <effa.kelpo@gmail.com>
2021-02-23 00:28:39 +0800

    Upload Project

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout-admin.index')
+@section('title', 'Dashboard')
+@section('content')
+<div class="container-fluid p-0">
+    <div class="row mb-2 mb-xl-3">
+        <div class="col-auto d-none d-sm-block">
+            <h3><strong>Analytics</strong> Dashboard</h3>
+        </div>
+
+        <div class="col-auto ml-auto text-right mt-n1">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent p-0 mt-1 mb-0">
+                    <li class="breadcrumb-item"><a href="#">AdminKit</a></li>
+                    <li class="breadcrumb-item"><a href="#">Dashboards</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Analytics</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+@stop
